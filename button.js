b541017799790b97b5377e6c0e5f5632c7ab3a15
TkymHrt <s231053@stn.nagaokaut.ac.jp>
2023-05-24 01:08:25 +0900

    add button

---
+++ button.js
@@ -0,0 +1,4 @@
+document.getElementById('flObgButton').addEventListener('click', () => {
+    const clickEvent = document.getElementById('floatingMenu');
+    clickEvent.classList.toggle('notshow');
+});
